c7f6b82e876cb74471acabf0db6d3905f6f9e71e
George Wyscaver <gwyscaver3@gmail.com>
2019-09-10 22:40:01 -0700

    add gif button to array completed

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-var topics = ["hotel", "airplane", "beach", "palm tree", "airport", "tourist", "crying kids", "florida"]
+var topics = ["hotel", "motel", "airplane", "beach", "palm tree", "airport", "tourist", "crying kids", "florida", "beach towel", "camping"];
 
 //create button for the page
 function createButtons() {
+    $(".buttonsDiv").empty(); 
     for (let i = 0; i < topics.length; i++) {
         var btn = $("<button>")
         btn.addClass("travelBtn")
@@ -11,7 +12,21 @@ function createButtons() {
         $(".buttonsDiv").append(btn)
     }
 }
-createButtons()
+    $("#add-gif").on("click", function(event) {
+      // event.preventDefault() prevents the form from trying to submit itself.
+      // We're using a form so that the user can hit enter instead of clicking the button if they want
+      event.preventDefault();
+    
+      // This line will grab the text from the input box
+      var inputGif = $("#travel-input").val().trim();
+      // The gif input from the textbox is then added to our array
+      topics.push(inputGif);
+    
+      // calling renderButtons which handles the processing of our movie array
+      createButtons();
+    });
+
+createButtons();
 
 //listening to click on buttons
 $(document).on("click", ".travelBtn", function () {
@@ -20,14 +35,14 @@ $(document).on("click", ".travelBtn", function () {
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=nDOjknhqowdvI7SpOGOaEwLfq7treK7f&limit=10";
     //console.log(topic)
     $.ajax({
-            url: queryURL,
-            method: "GET"
-        })
-        .then(function (response) {
-            var gifs = response.data;
-
-            console.log(gifs);
-            $("resultsDiv").empty();
+        url: queryURL,
+        method: "GET"
+    })
+    .then(function (response) {
+        var gifs = response.data;
+        
+        //console.log(gifs);
+        $(".resultsDiv").empty();
             for (let j = 0; j < gifs.length; j++) {
                 var gifDiv = $("<div>");
                 var gifRating = gifs[j].rating;
@@ -39,23 +54,17 @@ $(document).on("click", ".travelBtn", function () {
                 .attr("data-still", gifs[j].images.fixed_height_still.url)
                 .attr("data-state", "still");
 
-                
-
                 gifDiv.prepend(p);
                 gifDiv.prepend(travelImage);
 
                 $(".resultsDiv").prepend(gifDiv);
                 
                 travelImage.addClass ("gifPic")
-
                 
-                
-                
-            }
-            
-           
+            }      
         });
 });
+
 $(document).on("click", ".gifPic",function (){
     var state = $(this).attr("data-state");
     if(state === "still") {
@@ -69,3 +78,4 @@ $(document).on("click", ".gifPic",function (){
           } 
 
 });
+ 
